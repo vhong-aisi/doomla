85c232ecfe672e67ebc85f3159eba6cca21eb5f7
Vy.Hong <Vy.Hong@dsit.gov.uk>
2025-08-07 12:29:40 +0000

    Human cli example

--- task.py
+++ task.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
 from inspect_ai import Task, eval, task
-from inspect_ai.agent import react
+from inspect_ai.agent import human_cli
 from inspect_ai.scorer import includes
-from inspect_ai.tool import bash
 from inspect_cyber import create_agentic_eval_dataset
 
 
@@ -15,14 +14,14 @@ def doomla():
                 root_dir=Path("evals/doomla").resolve()
             ).filter_by_metadata({"variant_name": "example"})
         ),
-        solver=react(tools=[bash(30)]),
+        solver=human_cli(),
         scorer=includes(),
     )
 
 
 eval(
     doomla,
-    model="openai/o4-mini",
-    max_tokens=50_000,
+    # model="openai/o4-mini",
+    # max_tokens=5000,
     # epochs=3,
 )
